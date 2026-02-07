8a15fbddf9d64531aa8eb5164cacd2968d842a75
Fawwaz Ibrahim <158780885+faww6z@users.noreply.github.com>
2026-02-07 00:06:42 -0330

    Time: 19 ms (80.84%) | Memory: 16.2 MB (70.13%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        self.strs = strs
+        res = defaultdict(list)
+        for s in strs:
+            sortedS = ''.join(sorted(s))
+            res[sortedS].append(s)
+        return list(res.values())
+        
